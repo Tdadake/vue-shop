ad04d0b7bdb7bb47ea500f19d0f568303a3a104a
Tdadake <89580936+Tdadake@users.noreply.github.com>
2022-08-15 17:53:28 +0800

    Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,108 @@
 <template>
-  <div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png">
+      </div>
+      <!-- 登录表单区 -->
+      <el-form class="login_form" :model="loginForm" :rules="loginFormRules">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <!-- 双向绑定数据 -->
+          <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <!-- 双向绑定数据 -->
+          <el-input prefix-icon="iconfont icon-3702mima" type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+        // 登录表单的数据绑定对象
+        loginForm: {
+          username: '',
+          password:''
+        },
+        // 表单数据验证规则对象
+      loginFormRules: {
+          // 验证用户名合法性
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 18, message: '长度在 3 到 18 个字符', trigger: 'blur' }
+          ],
+          // 验证密码合法性 
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+          ]
+        }
+      };
+    },
 }
 </script>
 
 <style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+
+  .avatar_box{
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    // 2d转换偏移
+    transform: translate(-50%,-50%);
+    background-color: #fff;
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  // 边框包含在width内
+  box-sizing: border-box;
+}
+
+.btns{
+  // 弹性盒模型
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     redirect: '/login'
   },
   {
-    path: 'login',
+    path: '/login',
     component: Login
   }
 ]
